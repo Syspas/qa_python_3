dbcf031d4bcdf2589bda8db7596f3d7ee3c2528a
TestMasterBest <31808851+TestMasterBest@users.noreply.github.com>
2024-02-08 12:00:57 +0300

    Внесены изменения в код согласно коментариям ревюрера.

--- online_sales_register.py
+++ online_sales_register.py
@@ -8,7 +8,8 @@ class OnlineSalesRegisterCollector:
         self.__tax_rate = {'чипсы': 20, 'кола': 20, 'печенье': 20, 'молоко': 10, 'кефир': 10}
 
 #1. Напиши геттеры
-#В чеке печатают названия товаров и их количество. Но атрибуты name_items и number_items — приватные. К ним нельзя обратиться напрямую.
+#В чеке печатают названия товаров и их количество. Но атрибуты name_items и number_items — приватные.
+#К ним нельзя обратиться напрямую.
 #Напиши геттеры, которые получают значения name_items и number_items. Используй декоратор @property.
 
 
@@ -38,12 +39,12 @@ class OnlineSalesRegisterCollector:
             raise ValueError ('Нельзя добавить товар, если в его названии нет символов или их больше 40')
 #Если названия товара нет в списке item_price, выводится исключение
 # NameError с текстом 'Позиция отсутствует в товарном справочнике'.
-        if  name in self.__item_price:
+        if  name not in self.__item_price:
             raise NameError ('Позиция отсутствует в товарном справочнике')
         else:
 #В остальных случаях метод добавляет товар в name_items и увеличивает значение number_items на 1.
             self.__name_items.append(name)
-
+            self.__number_items+=1
 
 #3. Удали товар из чека
 #Напиши метод delete_item_from_check. Он убирает товары из чека. Метод принимает аргумент name.
@@ -53,11 +54,11 @@ class OnlineSalesRegisterCollector:
 
     def delete_item_from_check(self,name):
 #Если товара нет в списке name_items, выводится исключение NameError с текстом 'Позиция отсутствует в чеке';
-        if name in self.__name_items:
+        if name not in self.__name_items:
             raise NameError('Позиция отсутствует в чеке')
         else:
             self.__name_items.remove(name)
-
+            self.__number_items-=1
 
 #4. Посчитай общую стоимость товаров
 #Напиши метод check_amount. Он считает общую сумму покупок.
